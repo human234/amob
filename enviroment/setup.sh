0a4db10c57c0a7df3cb08b44ed7da51647052455
human234 <so988368442@gmail.com>
2025-09-19 16:14:37 +0800

    the end

--- enviroment/setup.sh
+++ enviroment/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-mkdir -p .data .plugins .db .config .grafana-provisioning .ollama .openwebui
+sudo rm -rf .influxdb3-data .influxdb3-plugins .explorer-db .explorer-config .grafana-provisioning .ollama .openwebui
+docker compose down
+mkdir -p .influxdb3-data .influxdb3-plugins .explorer-db .explorer-config .grafana-provisioning .ollama .openwebui
 docker compose up -d
 docker container exec influxdb3-core influxdb3 create token --admin > .tokens
